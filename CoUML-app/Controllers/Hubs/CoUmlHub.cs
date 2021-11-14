6e3d6ad723ffa7dedeb0e8877650edfde7073fc1
mySmurph <56486787+mySmurph@users.noreply.github.com>
2021-11-14 09:43:12 -0800

    everything works
    run npm install in CoUML-app

--- CoUML-app/Controllers/Hubs/CoUmlHub.cs
+++ CoUML-app/Controllers/Hubs/CoUmlHub.cs
@@ -22,7 +22,17 @@ namespace CoUML_app.Controllers.Hubs
 /// data structure to store active users
     public class ConnectionMap<T>
     {
+        /// <summary>
+        /// the repo of active connections
+        /// </summary>
+        /// <typeparam name="T">connectionId: string</typeparam>
+        /// <typeparam name="T">value: string</typeparam>
         private readonly Dictionary<T, T> _connections = new Dictionary<T, T>();
+
+        /// <summary>
+        /// get the count of client connections in the repo
+        /// </summary>
+        /// <value>int</value>
         public int Count
         {
             get
@@ -31,6 +41,16 @@ namespace CoUML_app.Controllers.Hubs
             }
         }
         
+        /// <summary>
+        /// add a client connection to the repo
+        /// </summary>
+        /// <param name="connectionId">
+        ///     the client id retrieved from the hub context
+        ///     > Context.ConnectionId
+        /// </param>
+        /// <param name="value">
+        ///     \! not currently used - a value to associate with the connectionId
+        /// </param>
         public void Add(T connectionId, T value)
         {
             lock(_connections)
@@ -40,6 +60,11 @@ namespace CoUML_app.Controllers.Hubs
             }
         }
 
+        /// <summary>
+        /// removes a client connection from the repo
+        /// </summary>
+        /// <param name="connectionId">the id of the connection</param>
+        /// <returns>true if sucsessfully removed</returns>
         public bool Remove(T connectionId)
         {
             bool isItemRemoved = false;
@@ -51,6 +76,11 @@ namespace CoUML_app.Controllers.Hubs
             return isItemRemoved;
         }
 
+        /// <summary>
+        /// determin if the connectioId is is in the repo
+        /// </summary>
+        /// <param name="connectionId">the connectionId of the client connection</param>
+        /// <returns>true: connectionId is in repo</returns>
         public bool IsConnected(T connectionId)
         {
             T temValueHolder;
@@ -61,9 +91,19 @@ namespace CoUML_app.Controllers.Hubs
     }
     public class CoUmlHub : Hub<ICoUmlClient>
     {
+        /// <summary>
+        /// connection repo
+        /// </summary>
+        /// <typeparam name="string"></typeparam>
+        /// <returns></returns>
         private readonly static ConnectionMap<string> _connections = new ConnectionMap<string>();
         
+        
+        /// <summary>
         /// when a new user attemps to connect their connection id gets logged in the connection repo
+        /// and calls test connection
+        /// </summary>
+        /// <returns></returns>
         public override Task OnConnectedAsync()
         {
             string connectionId = Context.ConnectionId;
@@ -75,7 +115,11 @@ namespace CoUML_app.Controllers.Hubs
             return base.OnConnectedAsync();
         }
 
+        /// <summary>
         /// remove connection from repo apon disconnection
+        /// </summary>
+        /// <param name="exception"> \!not used </param>
+        /// <returns></returns>
         public override Task OnDisconnectedAsync(Exception exception)
         {
             string connectionId = Context.ConnectionId;
@@ -83,6 +127,10 @@ namespace CoUML_app.Controllers.Hubs
             return base.OnDisconnectedAsync(exception);
         }
 
+        /// <summary>
+        /// test server to client communication
+        /// </summary>
+        /// <param name="connectionid">connectionId of client being called</param>
         public void TestCall(string connectionid)
         {
             Clients.Client(connectionid).testInterfaceMethod("this is the test message :D");
