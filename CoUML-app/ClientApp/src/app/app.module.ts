1b4184ab53c315c4c19691fa56c02e2476696a8c
mySmurph <56486787+mySmurph@users.noreply.github.com>
2021-11-13 17:32:11 -0800

    update version numbers

--- CoUML-app/ClientApp/src/app/app.module.ts
+++ CoUML-app/ClientApp/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 import { CoUmlHubService } from './service/couml-hub.service';
-import {ButtonModule} from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -20,8 +20,7 @@ import {ButtonModule} from 'primeng/button';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
-    ButtonModule
+    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -31,7 +30,9 @@ import {ButtonModule} from 'primeng/button';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    
+    ButtonModule
   ],
   providers: [
     CoUmlHubService
