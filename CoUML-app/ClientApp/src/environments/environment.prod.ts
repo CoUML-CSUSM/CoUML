93ebae9f9ba9aa88726bfd7f73cbd03cea294eec
mySmurph <56486787+mySmurph@users.noreply.github.com>
2022-04-13 11:11:38 -0700

    ip

--- CoUML-app/ClientApp/src/environments/environment.prod.ts
+++ CoUML-app/ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // TODO: change the ip of the apiUrl to the host machines ip/url
 export const environment = {
   production: true,
-  apiUrl: "https://54.241.128.89"
+  apiUrl: "https://54.241.128.89.xip.io"
 };
